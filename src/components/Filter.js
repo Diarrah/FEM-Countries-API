53bf0573e53d30bec32914666adc5691edf23a2a
Diarrah <diarrahcasiano@gmail.com>
2020-08-19 19:39:05 -0700

    Drop down list to filter countries on home page

---
+++ src/components/Filter.js
@@ -0,0 +1,37 @@
+import React, { useState, useContext } from 'react';
+import { CountriesContext } from '../contextHelpers/countryContext';
+
+const Filter = () => {
+    const { filterByRegion, setFilterByRegion } = useContext(CountriesContext);
+    const [droppedDown, setDroppedDown] = useState(false);
+    const listItem = [...document.querySelectorAll('.filter__box__list__item')]; 
+
+    const showFilterOptions = () => {
+        setDroppedDown(!droppedDown)
+    }
+
+    listItem.forEach(region => region.addEventListener('click', function(e) {
+        setFilterByRegion(e.target.dataset.region)
+        setDroppedDown(!droppedDown)
+    }));
+
+    return (
+        <div className="filter__box">
+            <button className="filter__box__deployment" onClick={() => showFilterOptions()}>
+            <span>{filterByRegion !== '' ? `${filterByRegion}` : `Filter By Region`}</span>
+                <i className="fas fa-chevron-down"></i>
+            </button>
+            <ul className={`filter__box__list ${droppedDown ? 'active' : ''}`}>
+                {filterByRegion && <li className="filter__box__list__item" data-region="">All</li>}
+                <li className="filter__box__list__item" data-region="Africa">Africa</li>
+                <li className="filter__box__list__item" data-region="Americas">Americas</li>
+                <li className="filter__box__list__item" data-region="Asia">Asia</li>
+                <li className="filter__box__list__item" data-region="Europe">Europe</li>
+                <li className="filter__box__list__item" data-region="Oceania">Oceania</li>
+                <li className="filter__box__list__item" data-region="Polar">Polar</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Filter;
