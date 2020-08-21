d7c6b56892351b4c53339cf421b9b8311d869a7b
Diarrah <diarrahcasiano@gmail.com>
2020-08-20 22:32:00 -0700

    Routing to both pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import CountriesContextProvider from './contextHelpers/countryContext';
+import { Home, Country } from './MainPages';
+import { Header } from './components';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <CountriesContextProvider>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/country/:countryCode" component={Country} />
+          </Switch>
+        </div>
+      </CountriesContextProvider>
+    </Router>
   );
 }
 
