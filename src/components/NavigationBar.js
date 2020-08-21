35f11313b1b9a190255d063102652e902f202bf4
Diarrah <diarrahcasiano@gmail.com>
2020-08-20 20:25:58 -0700

    Changed to implicit returns

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Search, Filter } from '../components';
 
-const NavigationBar = () => {
-    return (
-        <div className="navigation__bar">
-            <Search />
-            <Filter />
-        </div>
-    )
-}
+const NavigationBar = () => (
+    <div className="navigation__bar">
+        <Search />
+        <Filter />
+    </div>
+)
 
 export default NavigationBar;
